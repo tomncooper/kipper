9d22e3ef38e94c980736c053995fe6d0637240d8
Thomas Cooper <code@tomcooper.dev>
2024-12-29 17:17:39 +0000

    Added flip wiki summary table parsing

--- ipper/common/constants.py
+++ ipper/common/constants.py
@@ -1,12 +1,14 @@
 from enum import StrEnum
 
 UNKNOWN_STR: str = "unknown"
+NOT_SET_STR: str = "not set"
 
 
 class IPState(StrEnum):
     """Enum representing the status of an improvement proposal."""
 
+    RELEASED: str = "released"
     ACCEPTED: str = "accepted"
     UNDER_DISCUSSION: str = "under discussion"
     NOT_ACCEPTED: str = "not accepted"
-    UNKNOWN: str = "unknown"
+    UNKNOWN: str = UNKNOWN_STR
--- ipper/flink/wiki.py
+++ ipper/flink/wiki.py
@@ -1,11 +1,11 @@
 import re
 
-from typing import Any, Optional, Union
+from typing import Any, Optional, cast, Union
 
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 
-from ipper.common.constants import IPState, UNKNOWN_STR
+from ipper.common.constants import IPState, UNKNOWN_STR, NOT_SET_STR
 from ipper.common.wiki import (
     APACHE_CONFLUENCE_BASE_URL,
     get_wiki_page_info,
@@ -13,6 +13,15 @@ from ipper.common.wiki import (
 )
 
 KIP_PATTERN: re.Pattern = re.compile(r"FLIP-(?P<flip>\d+)", re.IGNORECASE)
+RELEASE_NUMBER_PATTERN: re.Pattern = re.compile(r"\d+\.?\d*")
+
+TEMPLATE_BOILER_PLATE_PREFIX = "here (<-"
+
+DISCUSSION_THREAD_KEY = "discussion_thread"
+VOTE_THREAD_KEY = "vote_thread"
+JIRA_ID_KEY = "jira_id"
+JIRA_LINK_KEY = "jira_link"
+RELEASE_KEY = "release"
 
 
 def get_flip_main_page_info(timeout: int = 30) -> dict[str, Any]:
@@ -25,52 +34,166 @@ def get_flip_main_page_info(timeout: int = 30) -> dict[str, Any]:
     )
 
 
-def get_current_state(para):
-    return None
+def _find_Jira_key_and_link(row_data: Tag) -> tuple[Optional[str], Optional[str]]:
+
+    jira_div = row_data.find("div", {"class": "content-wrapper"})
+
+    if jira_div:
+        jira_span = cast(Tag, jira_div).find(
+            "span",
+            {"class": "jira-issue conf-macro output-block"},
+        )
+    else:
+        return None, None
+
+    if jira_span:
+        if cast(Tag, jira_span).has_attr("data-jira-key"):
+            jira_id_values: Optional[Union[list[str], str]] = cast(Tag, jira_span).get(
+                "data-jira-key"
+            )
+            if isinstance(jira_id_values, list):
+                jira_id = jira_id_values[0]
+            else:
+                jira_id = cast(str, jira_id_values)
+        else:
+            jira_id = None
+
+        link = jira_span.find("a")
+        if link and cast(Tag, link).has_attr("href"):
+            jira_link_values: Optional[Union[list[str], str]] = cast(Tag, link).get("href")
+            if isinstance(jira_link_values, list):
+                jira_link = jira_link_values[0]
+            else:
+                jira_link = cast(str, jira_link_values)
+        else:
+            jira_link = None
+
+        return jira_id, jira_link
+
+    return None, None
 
 
-def enrich_flip_info(body_html: str, flip_dict: dict[str, Union[str, int]]) -> None:
+def _add_row_data(header: str, row_data: Tag, flip_dict: dict[str, Union[str, int]]) -> None:
+
+    if "discussion" in header:
+        if TEMPLATE_BOILER_PLATE_PREFIX in row_data.text:
+            flip_dict[DISCUSSION_THREAD_KEY] = NOT_SET_STR
+            return
+
+        link = row_data.find("a")
+        if link and cast(Tag, link).has_attr("href"):
+            flip_dict[DISCUSSION_THREAD_KEY] = cast(Tag, link).get("href")
+        else:
+            flip_dict[DISCUSSION_THREAD_KEY] = NOT_SET_STR
+
+        return
+
+    if "vote" in header:
+        if TEMPLATE_BOILER_PLATE_PREFIX in row_data.text:
+            flip_dict[VOTE_THREAD_KEY] = NOT_SET_STR
+            return
+
+        link = row_data.find("a")
+        if link and cast(Tag, link).has_attr("href"):
+            flip_dict[VOTE_THREAD_KEY] = link.get("href")
+        else:
+            flip_dict[VOTE_THREAD_KEY] = NOT_SET_STR
+
+        return
+
+    if "jira" in header:
+        if TEMPLATE_BOILER_PLATE_PREFIX in row_data.text:
+            flip_dict[JIRA_ID_KEY] = NOT_SET_STR
+            flip_dict[JIRA_LINK_KEY] = NOT_SET_STR
+            return
+
+        jira_id, jira_link = _find_Jira_key_and_link(row_data)
+
+        if jira_id:
+            flip_dict[JIRA_ID_KEY] = jira_id
+        else:
+            flip_dict[JIRA_ID_KEY] = NOT_SET_STR
+
+        if jira_link:
+            flip_dict[JIRA_LINK_KEY] = jira_link
+        else:
+            flip_dict[JIRA_LINK_KEY] = NOT_SET_STR
+
+        return
+
+    if "release" in header:
+
+        result = RELEASE_NUMBER_PATTERN.search(row_data.text)
+        if result:
+            release_number = result.group()
+            if "operator" in row_data.text:
+                release_number = "operator " + release_number
+            flip_dict[RELEASE_KEY] = release_number
+        else:
+            flip_dict[RELEASE_KEY] = NOT_SET_STR
+
+
+def _determine_state(flip_dict) -> None:
+
+    if flip_dict[RELEASE_KEY] != UNKNOWN_STR and flip_dict[RELEASE_KEY] != NOT_SET_STR:
+        flip_dict["state"] = IPState.RELEASED
+        return
+
+    # TODO: figure out the rest of the state algorithm
+
+def _enrich_flip_info(flip_id: int, body_html: str, flip_dict: dict[str, Union[str, int]]) -> None:
     """Parses the body of the FLIP wiki page pointed to by the 'content_url'
     key in the supplied dictionary. It will add the derived data to the
-    supplied dict."""
+    supplied dict.
 
-    parsed_body: BeautifulSoup = BeautifulSoup(body_html, "html.parser")
+    Search process:
+        1. Find the first table in the body (some flips don't have a table and will be ignored)
+        2. Identify if there is a Discussion Thread, Vote Thread, JIRA or Release entry. 
+           Add the details to the flip_dict.
+        3. If there is a release, set the status as RELEASED.
+        4. 
+    """
 
-    state_processed: bool = False
-    jira_processed: bool = False
+    parsed_body: BeautifulSoup = BeautifulSoup(body_html, "html.parser")    
 
-    for para in parsed_body.find_all("p"):
+    tables = parsed_body.find_all("table")
 
-        if not state_processed and "current state" in para.text.lower():
-            state: Optional[str] = get_current_state(para.text)
-            if state:
-                flip_dict["state"] = state
-            else:
-                print(f"Could not discern KIP state from {para}")
-                flip_dict["state"] = IPState.UNKNOWN
+    # Setup the status entries to default unknown
+    flip_dict[DISCUSSION_THREAD_KEY] = UNKNOWN_STR
+    flip_dict[VOTE_THREAD_KEY] = UNKNOWN_STR
+    flip_dict[RELEASE_KEY] = UNKNOWN_STR
+    flip_dict["state"] = IPState.UNKNOWN
 
-            state_processed = True
+    if not tables:
+        print(
+            f"WARNING: no summary table in FLIP-{flip_id}. " +
+            f"This FLIP state will be set to {UNKNOWN_STR}."
+        )
+        return
 
-        elif not jira_processed and "jira" in para.text.lower():
-            link: Tag = para.find("a")
-            if link:
-                href: Optional[Union[list[str], str]] = link.get("href")
-            else:
-                href = None
+    # We assume that the first table on the page is the summary table
+    summary_table = tables[0]
+    summary_rows = summary_table.findAll("tr")
+    if not summary_rows:
+        print(
+            f"WARNING: no information in summary table in FLIP-{flip_id}. " +
+            f"This FLIP state will be set to {UNKNOWN_STR}."
+        )
+        return
 
-            if href:
-                flip_dict["jira"] = href
-            else:
-                print(f"Could not discern JIRA link from {para}")
-                flip_dict["jira"] = UNKNOWN_STR
+    for row in summary_rows:
+        header_tag = row.find("th")
+        if header_tag:
+            header = header_tag.text.lower()
+        else:
+            # We have no idea what this row is
+            continue
 
-            jira_processed = True
+        row_data = row.find('td')
+        if row_data:
+            _add_row_data(header, row_data, flip_dict)
 
-    if not state_processed:
-        flip_dict["state"] = IPState.UNKNOWN
-
-    if not jira_processed:
-        flip_dict["jira"] = UNKNOWN_STR
+    _determine_state(flip_dict)
 
 
 def process_child_kip(flip_id: int, child: dict):
@@ -78,7 +201,7 @@ def process_child_kip(flip_id: int, child: dict):
 
     print(f"Processing FLIP {flip_id} wiki page")
     child_dict: dict[str, Union[int, str]] = {}
-    child_dict["flip_id"] = flip_id
+    child_dict["id"] = flip_id
     child_dict["title"] = child["title"]
     child_dict["web_url"] = APACHE_CONFLUENCE_BASE_URL + child["_links"]["webui"]
     child_dict["content_url"] = child["_links"]["self"]
@@ -88,7 +211,7 @@ def process_child_kip(flip_id: int, child: dict):
     child_dict["last_modified_by"] = child["history"]["lastUpdated"]["by"][
         "displayName"
     ]
-    #enrich_flip_info(child["body"]["view"]["value"], child_dict)
+    _enrich_flip_info(flip_id, child["body"]["view"]["value"], child_dict)
 
     return child_dict
 
